cbe9c724a70dcaeeafc8973db897eb904b4f73ba
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-09-17 21:20:54 +0530

    make role dto

---
+++ src/main/java/lk/ijse/dto/RoleDTO.java
@@ -0,0 +1,38 @@
+package lk.ijse.dto;
+
+public class RoleDTO {
+    private int roleId;
+    private String roleName;
+
+    public RoleDTO() {
+    }
+
+    public RoleDTO(int roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleDTO{" +
+                "roleId=" + roleId +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
+}
