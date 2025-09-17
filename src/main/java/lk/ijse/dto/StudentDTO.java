bad9b8ca67aa7f8a070d2a21cb9ea4a04a15b50d
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-09-17 22:14:41 +0530

    make student dto

---
+++ src/main/java/lk/ijse/dto/StudentDTO.java
@@ -0,0 +1,88 @@
+package lk.ijse.dto;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class StudentDTO {
+    private int studentId;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private Date dob;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(int studentId, String name, String email, String phone, String address, Date dob) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.dob = dob;
+    }
+
+    // Getters and Setters
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentDTO that = (StudentDTO) o;
+        return studentId == that.studentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId);
+    }
+}
