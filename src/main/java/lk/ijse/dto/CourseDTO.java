cc3a5c84705fd096e09e930092abf72538ac8215
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-09-17 22:55:59 +0530

    make course dto

---
+++ src/main/java/lk/ijse/dto/CourseDTO.java
@@ -0,0 +1,48 @@
+package lk.ijse.dto;
+
+public class CourseDTO {
+    private int courseId;
+    private String name;
+    private int duration;
+    private double fee;
+    private int instructorId;
+    private String instructorName;
+
+    public CourseDTO() {
+    }
+
+    public CourseDTO(int courseId, String name, int duration, double fee, int instructorId, String instructorName) {
+        this.courseId = courseId;
+        this.name = name;
+        this.duration = duration;
+        this.fee = fee;
+        this.instructorId = instructorId;
+        this.instructorName = instructorName;
+    }
+
+    // Getters and Setters
+    public int getCourseId() { return courseId; }
+    public void setCourseId(int courseId) { this.courseId = courseId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public int getDuration() { return duration; }
+    public void setDuration(int duration) { this.duration = duration; }
+    public double getFee() { return fee; }
+    public void setFee(double fee) { this.fee = fee; }
+    public int getInstructorId() { return instructorId; }
+    public void setInstructorId(int instructorId) { this.instructorId = instructorId; }
+    public String getInstructorName() { return instructorName; }
+    public void setInstructorName(String instructorName) { this.instructorName = instructorName; }
+
+    @Override
+    public String toString() {
+        return "CourseDTO{" +
+                "courseId=" + courseId +
+                ", name='" + name + '\'' +
+                ", duration=" + duration +
+                ", fee=" + fee +
+                ", instructorId=" + instructorId +
+                ", instructorName='" + instructorName + '\'' +
+                '}';
+    }
+}
