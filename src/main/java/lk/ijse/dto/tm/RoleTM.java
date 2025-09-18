75e16eee7d73d6fa75ecfd560bf3ea4dc54ef090
Dilum Shamilka Pahasara <dilumshamilka4@gmail.com>
2025-09-18 08:07:52 +0530

    make role tm

---
+++ src/main/java/lk/ijse/dto/tm/RoleTM.java
@@ -0,0 +1,30 @@
+package lk.ijse.dto.tm;
+
+public class RoleTM {
+    private int roleId;
+    private String roleName;
+
+    public RoleTM() {
+    }
+
+    public RoleTM(int roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
